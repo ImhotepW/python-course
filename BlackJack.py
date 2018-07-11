765ce56cea7a27f74baaa9a7bc6f02350160fead
ImhotepW <al.vasilenko@gmail.com>
2018-07-11 19:18:10 +0300

    Pylint enhancements
    Changing code for better scoring

--- BlackJack.py
+++ BlackJack.py
@@ -1,24 +1,27 @@
+'''
+Text mode Black Jack game 
+'''
 from IPython.display import clear_output
 from Deck import Deck
 from Hand import Hand
 
-def request_bet(balance):
+def request_bet(curr_bal):
     '''This function requests the player to bet some amount of the balance '''
     the_bet = 0
-    while the_bet == 0: 
+    while the_bet == 0:
         try:
             the_bet = int(input('Place your bet: '))
-            if the_bet > balance:
+            if the_bet > curr_bal:
                 the_bet = 0
-                print('Not enough balance!')
+                print('Not enough curr_bal!')
         except ValueError:
             print('Please, input ingeger value!')
-    return the_bet    
+    return the_bet
 
-def questing(message ,variants):
+def questing(message, variants):
     '''Ask player something in message
-    OUTPUT: return True if player choose first two of provided variants, otherwise False
-    '''
+        OUTPUT: return True if player choose first two of provided variants, otherwise False
+        '''
     answer = ''
     while answer not in variants:
         answer = input(message).lower()
@@ -44,8 +47,8 @@ while True:
     print(player)
     
     # Player turn
-    while player.score < 21 and questing('How do you like it? Whant to (H)it or (S)tand? ', ['h','hit','s','stand']):
-        player.hit(deck.peek())        
+    while player.score < 21 and questing('What next? (H)it or (S)tand? ', ['h', 'hit', 's', 'stand']):
+        player.hit(deck.peek())
         #print('\n'*100)
         clear_output()
         print("Your balance is {}".format(balance))
@@ -53,10 +56,10 @@ while True:
         print(player)
         if player.score > 21:
             break
-    
-    # Dealer turn
-    while dealer.score <= 21 and player.score < 21:
-        dealer.hit(deck.peek())
+
+# Dealer turn
+while dealer.score <= 21 and player.score < 21:
+    dealer.hit(deck.peek())
     
     # Final deck
     clear_output()
@@ -72,22 +75,23 @@ while True:
     elif dealer.score > 21:
         print('Bust! Dealer loose... You WIN {}!!!'.format(bet))
         balance += bet
-    elif (dealer.score == player.score):
-        print('Push! Your bet is back...')
-        balance += bet
-    elif dealer.score > player.score :
+elif dealer.score == player.score:
+    print('Push! Your bet is back...')
+    balance += bet
+    elif dealer.score > player.score:
         print('Dealer WIN! Sorry, but you loose your bet...')
-    elif dealer.score < player.score :
-        print('Congratulations, you WIN {}!!!'.format(bet))
-        balance += bet
+elif dealer.score < player.score:
+    print('Congratulations, you WIN {}!!!'.format(bet))
+    balance += bet
     else:
         pass
 
-    if balance == 0 or questing('Would you like to (L)eave or (S)tay and contunue? ',['l','leave','s','stay']):
-        print('Thank you for playing with us!')
-        print('Your final balance is {}'.format(balance))
-        print('Good Bye!')
-        break
+if balance == 0 or questing('Would you like to (L)eave or (S)tay and contunue? ', ['l', 'leave', 's', 'stay']):
+    print('Thank you for playing with us!')
+    print('Your final balance is {}'.format(balance))
+    print('Good Bye!')
+    break
     else:
-        #print('\n'*100) 
+        #print('\n'*100)
         clear_output()
+
--- Hand.py
+++ Hand.py
@@ -1,20 +1,22 @@
-
+'''
+Hand class handles player's cards in the hand, can hit, print all cards in a hand and calculate score
+'''
 class Hand:
     '''
-    Class to represent player's hand 
+    Class to represent player's hand
     '''
 
     def __init__(self, name, cards=[]):
         '''
-        To initialise player's hand you need to provide a list with tuples 
+        To initialise player's hand you need to provide a list with tuples
         For instance [(2, '♠'), ([)'A', '♥')]
         '''
         self.name = name
         self.cards = cards
         self.score = self.get_score()
 
-    
-    def print_hand(self, hide_last = False):
+
+    def print_hand(self, hide_last=False):
         '''
         Draws a hand in a readable form
         INPUTS: if hide_last is True last card of the Hand will be printed face down
@@ -22,40 +24,40 @@ class Hand:
         '''
         print('{} hand:'.format(self.name))
         cards_cnt = len(self.cards)
-        prnt  =     ' _____    '*len(self.cards)+'\n'
-        prnt +=     '|     |   '*len(self.cards)+'\n'
+        prnt = ' _____    '*len(self.cards)+'\n'
+        prnt += '|     |   '*len(self.cards)+'\n'
         for i in range(0, cards_cnt):
-            prnt += '|  {}  |   '.format('*' if (hide_last and i==cards_cnt-1) else self.cards[i][0])
+            prnt += '|  {}  |   '.format('*' if (hide_last and i == cards_cnt-1) else self.cards[i][0])
         prnt += '\n'
-        prnt +=     '|     |   '*len(self.cards)+'\n'
+        prnt += '|     |   '*len(self.cards)+'\n'
         for i in range(0, cards_cnt):
-            prnt += '|  {}  |   '.format('*' if (hide_last and i==cards_cnt-1) else self.cards[i][1])
+            prnt += '|  {}  |   '.format('*' if (hide_last and i == cards_cnt-1) else self.cards[i][1])
         prnt += '\n'
-        prnt +=     '|_____|   '*len(self.cards)+'\n'
+        prnt += '|_____|   '*len(self.cards)+'\n'
 
         return prnt
-    
+
     def hit(self, card):
         '''Peek card to the Hand'''
-        self.cards += card  
+        self.cards += card
         self.score = self.get_score()
-    
+
     def get_score(self):
         '''Returns overall score of the player's hand'''
         score = 0
-        for value, suit in self.cards:
-            if value in ['T', 'J', 'Q', 'K']:
+        for card in self.cards:
+            if card[0] in ['T', 'J', 'Q', 'K']:
                 score += 10
-            elif value == 'A':
+            elif card[0] == 'A':
                 if score + 11 <= 21:
                     score += 11
                 else:
                     score += 1
             else:
-                score += value
+                score += card[0]
         return score
-            
-    
+
+
     def __str__(self):
         '''String representation of the Hand class'''
         return self.print_hand()
